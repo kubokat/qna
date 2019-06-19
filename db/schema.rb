8c880f5dca7bd88c4bebcee2983a2779fc329558
vladislavChe <kubokat@gmail.com>
2019-06-19 14:32:31 +0500

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_160330) do
+ActiveRecord::Schema.define(version: 2019_06_19_092305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_06_18_160330) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
