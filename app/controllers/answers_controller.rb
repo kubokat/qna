e8e0e22d9e9036e2089c6c90d3898ecb3d2a56aa
vladislavChe <kubokat@gmail.com>
2019-07-08 19:09:14 +0500

    4hw

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -42,6 +42,6 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body).merge(user_id: current_user.id)
   end
 end
